afbe05d6f458970b9c2ca1dc4eac27c64d575b93
Jonathan Cook <joncook@nmsu.edu>
2022-01-24 17:25:25 -0700

    added caps, started cmdline args

--- pullgitrepos.py
+++ pullgitrepos.py
@@ -1,40 +1,59 @@
 #!/usr/bin/python3
 
-import os;
+import os
+import argparse
 
 doGit = True
 doSvn = True
-testRun = True
+dryRun = True
+debug = False
+maxDepth = 3
 
-def processDir(cwd,indent):
-   print("{1}processing dir ({0})".format(cwd,indent))
+def processDir(cwd,depth):
+   if depth >= maxDepth: 
+      return False
+   indent = ""
+   for i in range(depth):
+      indent = indent+"   "
+   print("{1}dir ({0})".format(cwd,indent),end="")
    if os.path.exists(cwd):
       os.chdir(cwd)
    else:
       return False
-   print("{0}entered".format(indent))
+   if debug: print("{0}entered".format(indent))
    if os.path.exists(".svn"):
-      print("is root of an SVN repo, skipping")
-      if (not testRun and doSvn):
+      print("is an SVN repo, let's update")
+      if (not dryRun and doSvn):
          os.system("svn update")
       os.chdir("..")
       return True
    if os.path.exists(".git"):
-      print("{0}is root of a Git repo, lets pull".format(indent))
-      if (not testRun and doGit):
+      print("{0}is a Git repo, let's pull".format(indent))
+      if (not dryRun and doGit):
          os.system("git pull -a")
       os.chdir("..")
       return True
-   print("{0}not a Git repo".format(indent))
+   print("")
+   if debug: print("{0}not a Git repo".format(indent))
    sd = os.scandir()
    for f in sd:
       if f.is_dir():
-         processDir(f.name,indent+"   ")
+         processDir(f.name,depth+1)
    os.chdir("..")
-   print("{0}done".format(indent))
+   if debug: print("{0}done".format(indent))
    return False
 
+# 
+# Main
+#
+argParser = argparse.ArgumentParser(description='Git repo management')
+argParser.add_argument('--dry', help='perform dry run, no operations')
+argParser.add_argument('--svn', help='turn on svn update for svn repo')
+args = argParser.parse_args()
+print(args)
 
-processDir(os.path.abspath("."),"")
+if dryRun:
+   print("No action, just dry run")
+processDir(os.path.abspath("."),0)
 exit()
 
