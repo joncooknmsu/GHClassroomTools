6fc65f90662ee515e15633a4dcd077ba6af79f58
Jonathan Cook <joncook@nmsu.edu>
2024-01-29 14:46:48 -0700

    cleanup; add ghgetrepos.py

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # GHClassroomTools
 
-Tools and info for effectively using GitHub Classroom (and other Git repos)
+Tools and info for effectively using GitHub Classroom (and other Git repos). See header comments in scripts for usage.
 
-__getrepos.py__ is a script that will fetch all student repos, as specified by a CSV file.
+__getrepos.py__ is a script that will fetch all student repos, as specified by a simple CSV file.
+
+__ghgetrepos.py__ is a script that will fetch all student repos, as specified by a CSV file that is the format of the GitHub Classroom download files (you can download an assignment's "grade" file as CSV).
 
 __userstats.py__ is a script that generates user statistics of a repo (used to help evaluate contributions from student team members).
 
--- ghgetrepos.py
+++ ghgetrepos.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 #
-# Get Repos: uses a simple CSV input file to get student repositories
+# Get Repos: use a GitHub Classroom download format CSV input file
+# to get student repositories
 # - clones if the repo is not yet cloned, otherwise does a pull
 # - assumes that things are going to merge well and no interaction
 #   is needed
 #
-# NOTE: this script is mostly superceded by "ghgetrepos.py" which uses 
-#       the CSV file that you can download from GitHub Classroom for
-#       and assignment. TODO: the scripts should be merged.
-#
-# Usage: getrepos.py <input-file> [--room=<classroom>] [--assign=<assignment>]
+# Usage: getrepos.py <ghclassroom-assignment-csv-file>
 # 
 # input file is a CSV file, data input fields are:
 #
-#  LastName,FirstName,NMSUUserID,GitHubUserID
+#  assignment_name,assignment_url,starter_code_url,
+#  github_username,roster_identifier,student_repository_name,
+#  student_repository_url,submission_timestamp,points_awarded,
+#  points_available
 #
-# Actually, only the github user id is used
-# - TODO: rename the clone directory to be the students name?
-# - 
+# - this is the format that GitHub assignment grading files are in
+#
+# we use student_repository_name (6th field) to form a URL to clone
+#
+# TODO: make classroom a command line arg (done??, --room arg)
+# TODO: rename the clone directory to be the students name?
 #
 import os
 import sys
@@ -28,10 +31,15 @@ import subprocess
 
 # name of the GitHub organization that your classrooms fit into
 # - is part of the repo URL
-classroom = "NMSU-CS-Cook"
+#classroom = "NMSU-CS-Cook"
+classroom = "NMSU-CS-CS371"
+
+# NOT USED ANYMORE
 # name of the assignment (assignments will get confusing at the org
 # level if you do not prefix them with course and semester)
-assignment = "cs581-sp2022-individual"
+#assignment = "cs581-sp2022-individual"
+#assignment = "cs371-fa2022-individual"
+#git@github.com:NMSU-CS-Cook/cs-581-igloo-djarmas.git
 
 #-------------------------------------------------
 # If repo processing gets more complicated, move 
@@ -44,22 +52,30 @@ assignment = "cs581-sp2022-individual"
 def processRepoCSVFile(filename):
    global classroom, assignment
    f = open(filename,"r")
-   cf = csv.DictReader(f,fieldnames=('lastname', 'firstname', 'userid', 'githubuser'))
+   cf = csv.DictReader(f,fieldnames=('assignment_name', 'assignment_url', 'starter_code_url', 'github_username', 'roster_identifier', 'student_repository_name', 'student_repository_url', 'submission_timestamp', 'points_awarded',  'points_available'))
    for student in cf:
+      # skip header line
+      if student['assignment_name'] == 'assignment_name':
+          continue 
       try:
-         print("Student: {0},{1},{2}".format(student['firstname'], 
-                                      student['lastname'], student['githubuser']))
-         reponame = "{0}-{1}".format(assignment, student['githubuser'])
-         print("  Repo name: ({0})".format(reponame))
+         print("Student: {0},{1},{2}".format(student['github_username'], 
+                                      student['roster_identifier'], student['assignment_name']))
+         #reponame = "{0}-{1}".format(assignment, student['githubuser'])
+         reponame = student['student_repository_name']
+         # NOTE: we clone into a directory name that has the student
+         # roster id appended on it; I use the student's NMSU id 
+         # for this and so it connects me to the student in Canvas
+         dirname = reponame + "-" + student['roster_identifier']
+         #print("  Repo name: ({0})".format(reponame))
          repourl = "git@github.com:{0}/{1}.git".format(classroom, reponame)
          print("  Repo URL: ({0})".format(repourl))
          if repourl == "":
             print("  No repo, skipping")
             continue
          print("  Has repo: {0}".format(repourl))
-         if os.path.isdir(reponame):
+         if os.path.isdir(dirname):
             print("  Already cloned, pulling...")
-            os.chdir(reponame)
+            os.chdir(dirname)
             # --all flag will fetch all branches, not just current (master)
             # (but merging still happens on the current, I think; this 
             # usually works but if current branches are different (we 
@@ -68,7 +84,7 @@ def processRepoCSVFile(filename):
             os.chdir('..')
          else:
             print("  Cloning repo...")
-            subprocess.call(['git','clone',repourl])
+            subprocess.call(['git','clone',repourl,dirname])
       except:
          print("Exception: failed on {0}".format(student))
    f.close()
